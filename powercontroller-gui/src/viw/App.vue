626a8921807b2f10c52c8a2e4d06c971a32b30bc
kirk-zhang58 <zxc741208584@qq.com>
2025-05-30 23:39:35 +0800

    update vuejs

--- powercontroller-gui/src/viw/App.vue
+++ powercontroller-gui/src/viw/App.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+// export default 表示你要导出一个默认的对象，这样在其他文件中可以通过 import 语句来引入这个组件。
+// { name: 'app' } 是一个对象，其中的 name 属性用于给这个组件命名 , 通常是为了在脚本中选中这个组件名字
 export default {
   name: 'App'
 }
